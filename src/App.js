4808aa19c7f0e869b55aa0293146d53423d876fa
Kateryna Strough <katerynastrough@hotmail.com>
2020-03-01 23:54:07 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import AuthorInput from './components/AuthorInput';
 import Authors from './components/Authors';
 import BookInput from './components/BookInput';
 import Books from './components/Books';
-
-
 export class App extends Component {
   render() {
     return (
